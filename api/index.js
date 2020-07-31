63c62d81930ed4834dd766221a6fccfd5ce5a0a9
Your Name <your_email@real_email.edu>
2020-07-30 22:12:18 -0400

    JWT update-unfinished

--- api/index.js
+++ api/index.js
@@ -1,5 +1,38 @@
 const apiRouter = require("express").Router();
 
+const jwt = require("jsonwebtoken");
+const { getUserById } = require("../db");
+const { JWT_SECRET } = process.env;
+
+//  set `req.user` if possible
+apiRouter.use(async (req, res, next) => {
+  const prefix = "Bearer";
+  const auth = req.header("Authorization");
+
+  if (!auth) {
+    //nothing to see here
+    next();
+  } else if (auth.startsWidth(prefix)) {
+    const token = auth.slice(prefix.length);
+
+    try {
+      const { id } = jwt.verify(token, JWT_SECRET);
+
+      if (id) {
+        req.user = await getUserById(id);
+        next();
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  } else {
+    next({
+      name: "AuthorizationHeaderError",
+      message: `Authorization token must start with ${prefix}`,
+    });
+  }
+});
+
 const usersRouter = require("./users");
 apiRouter.use("/users", usersRouter);
 
